be058c50e29de6e09673354a9de04eec23bca87e
primerlee159 <57624649+primerlee159@users.noreply.github.com>
2024-06-09 22:39:22 +0800

    fill absent work into schedule

--- schedule.py
+++ schedule.py
@@ -1,5 +1,7 @@
 import os
 import openpyxl
+from openpyxl.styles import Font
+from openpyxl .styles.colors import Color
 import numpy as np
 
 class Schedule_EDIT():
@@ -14,7 +16,7 @@ class Schedule_EDIT():
         self.set_end_leader = [7,33]
         self.set_begin = [8,3]
         self.set_end = [19,33]
-        self.set_worker_row = -1
+        self.set_worker_row = []
         self.set_even_odd = -1
         self.ref_sort = ['N','A','B','A','B','1','2','3','4','5','6','7','8','9','10']
         self.new_data = []
@@ -26,6 +28,7 @@ class Schedule_EDIT():
     def get_values(self):
         arr = []
         arr_leader = []
+        get_worker_num = False
         for r,row in enumerate(self.sheet):
             arr2 = []
             arr2_leader = []
@@ -36,7 +39,10 @@ class Schedule_EDIT():
                     arr2_leader.append(col.value)
 
                 if col.value == '應上班人數':
-                    self.set_worker_row = r
+                    get_worker_num = True
+                elif get_worker_num:
+                    self.set_worker_row.append(col.value)
+                
                     
                 
             if r >= self.set_begin[0] and r <= self.set_end[0]:
@@ -57,7 +63,8 @@ class Schedule_EDIT():
         for r,row in enumerate(self.new_data):
             for c,cell in enumerate(row):
                 sheet.cell(r+self.set_begin[0]+1,c+self.set_begin[1]+1).value = cell
-
+                if cell == 'H' or cell == 'X':
+                    sheet.cell(r+self.set_begin[0]+1,c+self.set_begin[1]+1).font = Font(u'標楷體',color = "FFFF0000")
         wb_out.save(self.output_file_name)
 
     def create_ref(self):
@@ -125,18 +132,21 @@ class Schedule_EDIT():
             get_list[max_index] = 0
             return self.find_max(get_list)
         
-    def get_sort_element(get_list):
-        get_list = list(get_list)
-        for i,element in enumerate(get_list):
-            if element == 'B':
-                del get_list[i]
-                return element,get_list
-            elif element == 'A':
-                del get_list[i]
-                return element,get_list
-            elif element == 'X':
-                del get_list[i]
-                return element,get_list
+    def get_sort_element(self,get_list):
+        if 'B' in get_list:
+            get_list.remove('B')
+            return 'B'
+        elif 'A' in get_list:
+            get_list.remove('A')
+            return 'A'
+        elif 'N' in get_list:
+            get_list.remove('N')
+            return 'N'
+        else:
+            max_val = max(get_list)
+            get_list.remove(max_val)
+            return max_val
+
         
                     
     def collate_data(self):
@@ -151,12 +161,17 @@ class Schedule_EDIT():
                     if self.data[r,c] == 'H':
                         absent_work.append(self.new_data[r,c])
                         self.new_data[r,c] = 'H'
-                while True:
-                    print(self.new_data[self.find_max(self.new_data[:,c]),c])
-                    break
+
+                print(self.new_data[self.find_max(self.new_data[:,c]),c])
                 print(absent_work)
 
-
+                while absent_work != []:
+                    val = self.get_sort_element(get_list = absent_work)
+                    if val == 'N' or val == 'A' or val == 'B':
+                        self.new_data[self.find_max(self.new_data[:,c]),c] = val
+                    elif val < self.new_data[self.find_max(self.new_data[:,c]),c]:
+                        self.new_data[self.find_max(self.new_data[:,c]),c] = val
+                print(self.set_worker_row[c])
 
 if __name__ == '__main__':
     sch = Schedule_EDIT()
